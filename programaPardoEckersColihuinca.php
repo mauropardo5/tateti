7bb67de7a86b1dec29642e135889f57bed85ddd3
mauropardo5 <mauro.pardo@est.fi.uncoma.edu.ar>
2021-11-24 18:01:33 -0300

    Cambio de nombre archivo

--- programaPardoEckersColihuinca.php
+++ programaPardoEckersColihuinca.php
@@ -16,12 +16,58 @@ include_once("tateti.php");
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
+/** Función para el menu de opciones
+* @param int $numero
+* @return 
+*
+*/
+
+function seleccionarOpcion($numOpcion){
+
+    if ($numOpción == 1) {
+        function jugar()
+    };
+}
 
 
 
+/** Función para que el usuario ingrese un numero
+* @param int $num $min $max
+* @return int
+*
+*/
+
+function solicitarValor($min , $max){
+    $min = 1;
+    $max = 7;
+    echo "Ingrese un número de opción:";
+    $num = trim(fgets(STDIN));
+        if ($num <= $max || >= $max) {
+            echo "El número ingresado corresponde a una opción";
+        } else { 
+            echo "Ingrese un número válido";
+        };
+}
 
 
 
+/** Función para el menu de opciones
+* @param int $numero
+* @return int
+*
+*/
+function seleccionarOpcion($numero){
+echo " Menú de opciones:
+n/1) Jugar al tateti 
+n/2) Mostrar un juego 
+n/3) Mostrar el primer juego ganador 
+n/4) Mostrar porcentaje de Juegos ganados 
+n/5) Mostrar resumen de Jugador 
+n/6) Mostrar listado de juegos Ordenado por jugador O 
+n/7) salir 
+n/Elige una opción:
+"
+}
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/

06e42448cc1eb82e03afb059eafda52964776e83
mauropardo5 <mauro.pardo@est.fi.uncoma.edu.ar>
2021-11-24 18:19:16 -0300

    Funciones 1 y 2

--- programaPardoEckersColihuinca.php
+++ programaPardoEckersColihuinca.php
@@ -8,6 +8,11 @@ include_once("tateti.php");
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* ... COMPLETAR ... */
 
+/* Mauro Sebastian Pardo -  Legajo 3570 - MAIL: mauro.pardo@est.fi.uncoma.edu.ar - Usuario github: mauropardo5 */
+/* Colihuinca Maximiliano Ezequiel** - Legajo 3697 - MAIL: maximiliano.colihuinca@est.uncoma.edu.ar - Usuario: Zekecolin */
+/* Eckers Patricio - Legajo 3548 - MAIL: patricioeckers@gmail.com - Usuario: patricioeckersfai-3548 */
+
+
 
 
 
@@ -15,59 +20,55 @@ include_once("tateti.php");
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
-
-/** Función para el menu de opciones
-* @param int $numero
-* @return 
-*
-*/
-
-function seleccionarOpcion($numOpcion){
-
-    if ($numOpción == 1) {
-        function jugar()
-    };
-}
-
-
-
-/** Función para que el usuario ingrese un numero
-* @param int $num $min $max
-* @return int
-*
-*/
-
-function solicitarValor($min , $max){
+ 
+ 
+ 
+/** Modulo para solicitar numero
+ * @param int $min ; $max
+ * @return int
+ */
+$min = 1;
+$max = 7;
+ function solicitarValor(){
     $min = 1;
     $max = 7;
-    echo "Ingrese un número de opción:";
-    $num = trim(fgets(STDIN));
-        if ($num <= $max || >= $max) {
-            echo "El número ingresado corresponde a una opción";
-        } else { 
-            echo "Ingrese un número válido";
-        };
+do {
+    echo "Ingrese un número entre el 1 y el 7 ";
+    $numeroOpcion = trim(fgets(STDIN));
+    if($numeroOpcion >= $min && $numeroOpcion <= $max){
+        return $numeroOpcion;
+    }else{
+    echo "error en el valor ingresado, ingrese nuevamente el valor. ";
 }
-
-
-
+} while ($numeroOpcion < $min || $numeroOpcion > $max  );
+ 
+ 
+ }
+ 
+ 
 /** Función para el menu de opciones
-* @param int $numero
-* @return int
+* int $numero
+* return void
 *
 */
-function seleccionarOpcion($numero){
+function seleccionarOpción(){
 echo " Menú de opciones:
-n/1) Jugar al tateti 
-n/2) Mostrar un juego 
-n/3) Mostrar el primer juego ganador 
-n/4) Mostrar porcentaje de Juegos ganados 
-n/5) Mostrar resumen de Jugador 
-n/6) Mostrar listado de juegos Ordenado por jugador O 
-n/7) salir 
-n/Elige una opción:
-"
+     1) Jugar al tateti
+     2) Mostrar un juego
+     3) Mostrar el primer juego ganador
+     4) Mostrar porcentaje de Juegos ganados
+     5) Mostrar resumen de Jugador
+     6) Mostrar listado de juegos Ordenado por jugador O
+     7) salir
+     Elige una opción:
+    ";
+    $numero = SolicitarValor();
+ 
+return $numero;
 }
+ 
+$r= seleccionarOpción();
+echo $r;
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
